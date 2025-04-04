008d05881f08d464f4af7cc3ab283e07c2905e05
tdj4909 <tdj4909@gmail.com>
2025-04-04 22:03:33 +0900

    start spring boot

---
+++ src/main/java/com/papasong/bloomfesta/BloomfestaApplication.java
@@ -0,0 +1,13 @@
+package com.papasong.bloomfesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomfestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloomfestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/papasong/bloomfesta/BloomfestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papasong.bloomfesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomfestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
